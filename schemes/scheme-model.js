068d0553517dece4186fa1d70419cf022685e90c
irisjitomo <irisjitomo@eyahoo.com>
2019-10-09 16:45:55 -0400

    mvp on model.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -10,10 +10,10 @@ const db = knex(knexConfig.development)
 module.exports = {
     find,
     findById,
-    // findSteps,
-    // add,
-    // update,
-    // remove
+    findSteps,
+    add,
+    update,
+    remove
 }
 
 function find() {
@@ -21,5 +21,30 @@ function find() {
 }
 
 function findById(id){
-    return db('schemes').where('id', '=', id)
+    return db('schemes').where('id', '=', id).first()
+}
+
+function findSteps(id) {
+    return db('schemes as sc')
+    .select('sc.scheme_name', 's.step_number', 's.instructions')
+    .orderBy('s.step_number', 'asc')
+    .join('steps as s', 'sc.id', '=', 's.scheme_id')
+    .where('sc.id', id )
+}
+
+function add(scheme){
+    return db('schemes')
+    .insert(scheme, 'id')
+}
+
+function update(changes, id){
+    return db('schemes')
+    .where({id})
+    .update(changes)
+}
+
+function remove(id){
+    return db('schemes')
+    .where({id})
+    .del()
 }
